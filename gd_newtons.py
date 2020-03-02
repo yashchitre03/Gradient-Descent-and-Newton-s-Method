81e8f7cd80f0081c916a8ce6a63890a84b6cadb0
yashchitre03 <41328573+yashchitre03@users.noreply.github.com>
2020-03-01 20:18:20 -0600

    Changes in plot

--- gd_newtons.py
+++ gd_newtons.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-w = np.array([0.9, 0.05])
-eta = 0.007
+w = np.array([0.49, 0.49])
+eta = 0.001
 
 print(w, eta)
 
@@ -27,7 +27,7 @@ while (w[0]+w[1]) < 1 and w[0] > 0 and w[1] > 0 :
      
 all_weights = np.array(all_weights)
 
-plt.scatter(all_weights[:, 0], all_weights[:, 1])
+plt.plot(all_weights[:, 0], all_weights[:, 1], '.')
 plt.xlabel("X-axis")
 plt.ylabel("Y-axis")
 plt.xlim([0, 1])
@@ -35,16 +35,11 @@ plt.ylim([0, 1])
 plt.title("Gradient Descent Weight Updates")
 plt.show()
 
-plt.scatter(range(len(energy)), energy)
+plt.plot(range(len(energy)), energy)
 plt.xlabel("Number of Iterations")
 plt.ylabel("Energy f(w)")
 plt.title("Changes in Energy")
 plt.show()
-
-
-
-
-
 
 
 
@@ -80,7 +75,7 @@ while (w[0]+w[1]) < 1 and w[0] > 0 and w[1] > 0 :
         
         
 all_weights = np.array(all_weights)
-plt.scatter(all_weights[:, 0], all_weights[:, 1])
+plt.plot(all_weights[:, 0], all_weights[:, 1], '.')
 plt.xlabel("X-axis")
 plt.ylabel("Y-axis")
 plt.xlim([0, 1])
